72a374f286b82a6baa08ad57f443e6f02c4fbe5d
tunanosuke <gakkun1020@gmail.com>
2022-02-07 16:13:39 +0000

    画像投稿機能の作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f99a0067614d78309e2880b8fefc576e72f88a1b22abfdbce211417375d41015af8542adaf8a04ca36e802d0d9f3d4eb021a368d5eb9c2579c5dfa2dbfdbee35'
